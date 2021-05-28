12ae902463f10b79df6439e75dfbfb5ad0f8ea39
yosoyraul <yo.soy.raul@protonmail.com>
2021-05-27 20:21:04 -0400

    Added dynamodb to template. Changed lambda function to return acceptable response. Adapted to code to cloud i.e. removed localhost references.

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Serverless resume project inspired by the resume challange.
 https://acloudguru.com/blog/engineering/cloudguruchallenge-your-resume-in-azure
 
 
+In order to finalize SAM deployment, the id and hits values must be initialized in DynamoDB for the Lambda function to be operational.
 
+aws dynamodb put-item --table-name test --item '{\"id\":{\"N\":\"0\"},\"hits\": {\"N\":\"0\"}}'
--- src/lambda_function.py
+++ src/lambda_function.py
@@ -2,26 +2,43 @@ import json
 import os
 import boto3
 
+table_name = os.environ['TABLE_NAME']
+
+
+def increment_hits(tbl):
+    key = {'id': 0}            
+    expr ={':val': 1}
+    request = tbl.update_item(
+        Key=key,
+        UpdateExpression='SET hits= hits + :val',
+        ExpressionAttributeValues=expr
+        )
+    response = tbl.get_item(Key=key)
+    return response['Item']['hits']
 
 
 def lambda_handler(event, context):
     try:
-        dynamodb = boto3.resource('dynamodb',endpoint_url="http://dynamodb-local:8000")
-        table = dynamodb.Table('test')
-        key = {'id': 0}
-        expr ={':val': 1}
-        request = table.update_item(
-            Key=key,
-            UpdateExpression='SET hits= hits + :val',
-            ExpressionAttributeValues=expr
-        )
-        response = table.get_item(Key=key)
-        count = response['Item']['hits']
-        return count
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table(table_name)
+        table.wait_until_exists()
+
+        count = increment_hits(tbl=table)    
+
+        resp = {
+            "statusCode":200,
+            "headers":{
+                "Access-Control-Allow-Origin": "*"
+            },
+            "body":json.dumps({"count":int(count)})
+        }
+
+        return resp
+
     except Exception as e:
         print("Error processing event {}".format(json.dumps(event, indent=2)))
         raise e
 
-# Testing locally, requires dynamodb running locally on docker container with table and items already created.
+# Testing locally, requires dynamodb running locally on separate docker container with table and primary key already created.
 # sam local invoke -e events/apigateway_event.json ApiGatewayFunction --docker-network dynamodb_default
 # sam start-api --docker-network dynamodb_default
